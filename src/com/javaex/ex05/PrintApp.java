f51143a49804f21059241444baef6582b29110bb
kimwoojin1235 <ghkauszoqcu@naver.com>
2020-11-27 13:18:06 +0900

    수정

--- src/com/javaex/ex05/PrintApp.java
+++ src/com/javaex/ex05/PrintApp.java
@@ -10,7 +10,6 @@ public class PrintApp {
         print.printer(true);
         print.printer(5.7);
         print.printer("홍길동");
-
         
     }
 
--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -12,11 +12,11 @@ public class CConverterApp {
         CConverter.setRate(1118.70);
         
         //100만원을 달러로 출력하기
-        won=CConverter.toKWR(100.0);     
-        System.out.println(won);
-        //100달려를 원으로 출력하기
         dollar=CConverter.toDoller(1000000.0);
-        System.out.println(dollar);
+        System.out.println("백만원은"+dollar+"입니다.");
+        //100달려를 원으로 출력하기
+        won=CConverter.toKWR(100.0);     
+        System.out.println("백달러는"+won+"입니다.");
         
     }
 
